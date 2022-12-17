dbf6acadc77e65770abe2fe255b5f76ca0154df1
Sjors Blom <info@sjorsblom.com>
2022-12-17 13:56:14 +0100

    main models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField, DateField, EmailField, ForeignKey, DateTimeField, FloatField
+from django.db.models import Model, CASCADE
+import datetime
 
-# Create your models here.
+
+class User(AbstractUser):
+    """ Extention on django user model """
+    name = CharField()
+    age = DateField()
+    email = EmailField(unique=True) 
+    password = CharField()
+
+
+class Score(Model):
+    user = ForeignKey(User, on_delete=CASCADE)
+    date = DateTimeField(default=datetime.now, blank=True)
+    score = FloatField()
