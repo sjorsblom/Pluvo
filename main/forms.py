4840e3a389163c21120ac0751fdab0c9cab62f89
Sjors Blom <info@sjorsblom.com>
2022-12-19 14:26:32 +0100

    added forms

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.forms import Form, DateInput, PasswordInput, TextInput, CharField
+from django.forms import Form, DateInput, PasswordInput, TextInput, CharField, Select, FloatField, NumberInput
 from django.forms.fields import ChoiceField, DateField
 from main.models import User
 import datetime
@@ -7,19 +7,35 @@ from django.contrib.auth.forms import AuthenticationForm, UsernameField
 
 
 class ScoreSelectionForm(Form):
-    start = DateField(label="Grades from:", initial=lambda: datetime.date.today() - datetime.timedelta(days=7), required=True, widget=DateInput(attrs=dict(type='date')))
-    end = DateField(label="Grades until:", initial=datetime.date.today, required=True, widget=DateInput(attrs=dict(type='date')))
-    user = ChoiceField(label="User:", choices=[], required=True)
-
     def __init__(self, *args, **kwargs):
         super(ScoreSelectionForm, self).__init__(*args, **kwargs)
         self.fields['user'].choices = [(x.pk, x.username) for x in User.objects.all()]
+
+    start = DateField(label="", initial=lambda: datetime.date.today() - datetime.timedelta(days=7), required=True, widget=DateInput(
+        attrs={
+            'class': 'border-transparent',
+            'type': 'date'
+        }
+    ))
+    end = DateField(label="", initial=datetime.date.today, required=True, widget=DateInput(
+        attrs={
+            'class': 'border-transparent',
+            'type': 'date'
+        }))
+    user = ChoiceField(label="User:", choices=[], required=True, widget=Select(
+        attrs={
+            'class': 'w-36 rounded-md border-gray-300 shadow-sm focus:border-pluvo_green focus:ring-pluvo_green sm:text-sm',
+            'type': 'date'
+        }
+    ))
 
     def clean(self):
         cleaned_data = super(ScoreSelectionForm, self).clean()
         if cleaned_data['end'] < cleaned_data['start']:
             raise ValidationError("End date should come after the start date!!")
         return cleaned_data
+
+
 
 
 class UserLoginForm(AuthenticationForm):
@@ -37,3 +53,23 @@ class UserLoginForm(AuthenticationForm):
             'placeholder': 'Password'
         }
 ))
+
+
+class GradeForm(Form):    
+    def __init__(self, *args, **kwargs):
+        super(GradeForm, self).__init__(*args, **kwargs)
+        self.fields['user'].choices = [(x.pk, x.username) for x in User.objects.all()]
+
+    user = ChoiceField(label="User:", choices=[], required=True, widget=Select(
+        attrs={
+            'class': 'block w-full rounded-md border-gray-300 shadow-sm focus:border-pluvo_green focus:ring-pluvo_green sm:text-sm',
+            'type': 'date'
+        }
+    ))
+
+    grade = FloatField(label='', min_value=1, max_value=10, step_size=0.01, widget=NumberInput(
+        attrs={
+            'class': 'relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pluvo_green focus:outline-none focus:ring-pluvo_green sm:text-sm',
+            'placeholder': 'Grade'
+        }
+    ))
