26f3d8e060aeb9480ded4080013e08c4f8a05e11
Sjors Blom <info@sjorsblom.com>
2022-12-19 13:09:25 +0100

    cool login form

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,10 @@
 from django.core.exceptions import ValidationError
-from django.forms import Form, DateInput
+from django.forms import Form, DateInput, PasswordInput, TextInput, CharField
 from django.forms.fields import ChoiceField, DateField
 from main.models import User
 import datetime
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+
 
 class ScoreSelectionForm(Form):
     start = DateField(label="Grades from:", initial=lambda: datetime.date.today() - datetime.timedelta(days=7), required=True, widget=DateInput(attrs=dict(type='date')))
@@ -18,3 +20,20 @@ class ScoreSelectionForm(Form):
         if cleaned_data['end'] < cleaned_data['start']:
             raise ValidationError("End date should come after the start date!!")
         return cleaned_data
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(label='', widget=TextInput(
+        attrs={
+            'class': 'relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pluvo_green focus:outline-none focus:ring-pluvo_green sm:text-sm',
+            'placeholder': 'Username'
+        }))
+    password = CharField(label='', widget=PasswordInput(
+        attrs={
+            'class': 'relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pluvo_green focus:outline-none focus:ring-pluvo_green sm:text-sm',
+            'placeholder': 'Password'
+        }
+))
--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from main.forms import UserLoginForm
 
 app_name = 'main'
 urlpatterns = [
     # Authentication
     path('', views.dashboard_view, name='dashboard'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(authentication_form=UserLoginForm), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
